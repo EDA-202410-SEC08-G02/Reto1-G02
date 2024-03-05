d5f91b24abcff92d51f77c537d1da6cbd46b8b01
AndresFsantana <af.santana@uniandes.edu.co>
2024-03-04 21:35:26 -0500

    controlador

--- App/controller.py
+++ App/controller.py
@@ -132,12 +132,15 @@ def req_3(control):
     pass
 
 
-def req_4(control):
+def req_4(control, codigo_pais, fecha_inicial, fecha_final):
     """
     Retorna el resultado del requerimiento 4
     """
     # TODO: Modificar el requerimiento 4
-    pass
+    data_structs = control["model"]
+    data = model.req_4(data_structs,codigo_pais,fecha_inicial,fecha_final )
+    return data
+
 
 
 def req_5(control):
